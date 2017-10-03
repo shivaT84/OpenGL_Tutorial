52d478aa26e6e2b49527716c358a248efa9cbe74
shiva-T <tk_elt_0804@yahoo.co.jp>
2017-10-04 07:41:27 +0900

    はじめまして、おーぷんじーえるです

---
+++ OpenGLTutorial/OpenGLTutorial/Src/GLFWEW.cpp
@@ -0,0 +1,95 @@
+/** * @file GLFWE.cpp */
+#include "GLFWEW.h"
+#include <iostream>
+
+
+/// GLFW と GLEW をラップするための名前空間
+namespace GLFWEW { 
+	/**
+	* シングルトンインスタンスを取得する
+	*
+	* @return Window のシングルトンインスタンス
+	*/
+	Window& Window::Instance() {
+		static Window instance;
+		return instance;
+	}
+
+	/**
+	* コンストラクタ
+	*/
+	Window::Window() : isGLFWInitialized(false), isInitialized(false), window(nullptr) {
+	
+	}
+
+	/**
+	* デストラクタ
+	*/
+	Window::~Window() {
+		if (isGLFWInitialized) {
+			glfwTerminate();
+		}
+	}
+
+	/**
+	* GLFW/GLEW の初期化.
+	*
+	* @param w ウィンドウの描画範囲の幅(ピクセル).
+	* @param h ウィンドウの描画範囲の高さ(ピクセル).
+	* @param title ウィンドウタイトル(UTF-8 の 0 終端文字列).
+	*
+	* @retval true 初期化成功.
+	* @retval false 初期化失敗.
+	*/
+	bool Window::Init(int w, int h, const char* title) {
+		if (isInitialized) {
+			std::cerr << "ERROR: GLFWEW は既に初期化されています." << std::endl;
+			return false;
+		}
+		if (!isGLFWInitialized) {
+			//glfwSetErrorCallback(ErrorCallback);
+			if (glfwInit() != GL_TRUE) {
+				return false;
+			}
+			isGLFWInitialized = true;
+		}
+
+		if (!window) {
+			window = glfwCreateWindow(w, h, title, nullptr, nullptr);
+			if (!window) {
+				return false;
+			}
+			glfwMakeContextCurrent(window);
+		}
+
+		if (glewInit() != GLEW_OK) {
+			std::cerr << "ERROR: GLEW の初期化に失敗しました." << std::endl;
+			return false;
+		}
+
+		const GLubyte* renderer = glGetString(GL_RENDERER);
+		std::cout << "Renderer: " << renderer << std::endl;
+		const GLubyte* version = glGetString(GL_VERSION);
+		std::cout << "Version: " << version << std::endl;
+		isInitialized = true;
+		return true;
+	}
+
+	/**
+	* ウィンドウを閉じるべきか調べる.
+	*
+	* @retval true 閉じる.
+	* @retval false 閉じない.
+	*/
+	bool Window::ShouldClose() const {
+		return glfwWindowShouldClose(window) != 0;
+	}
+
+	/**
+	* フロントバッファとバックバッファを切り替える.
+	*/
+	void Window::SwapBuffers() const {
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+} // namespace GLFWEW 
